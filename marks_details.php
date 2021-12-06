de12df9f1798e093ec8c18f57a9b6fd04f28cf9f
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2021-12-06 21:23:58 +0530

    Update marks_details.php

--- marks_details.php
+++ marks_details.php
@@ -1,22 +1,42 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/marks_details.css">
     <title>Total Marks</title>
 </head>
 <body>
+    <?php 
+        $pinNumberErr = "";
+        $pinNumber = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["pinNumber"])) {
+                $pinNumberErr = "PIN is required";
+            } else {
+                $pinNumber = test_input($_POST["pinNumber"]);
+                if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                  $nameErr = "Only letters allowed";
+                }
+            }
+        }
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+          }
+    ?>
     <div class="container">
         <div class="title">
             <img src="images/vishnu_logo.bmp" alt="" />
         </div>
-        <div class="lbl">
-            <h3>PIN Number</h3>
-            <input type="text" />
-            <button>Search</button>
-        </div>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <div class="lbl">
+                <h3>PIN Number</h3>
+                <input type="text" name="pinNumber" />
+                <button name="submit" value="Submit">Search</button>
+            </div>
+        </form>
     </div>
 </body>
 </html>
